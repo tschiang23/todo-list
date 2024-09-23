9ca440a2f21271a9689ca1e8aa8f6bffea5dc4c8
tschiang23 <tien23@gmail.com>
2024-09-23 13:38:19 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+  }
 
 const app = express()
+
+const dbURI = process.env.MONGODB_URI
+mongoose.connect(dbURI)
+
+  // 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.get('/',(req,res)=>{
     res.send('Hello')
