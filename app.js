df96a4c9c4102b55e37db61f3b1fd68dddb87087
tschiang23 <tien23@gmail.com>
2024-09-25 11:49:09 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express')
 const mongoose = require('mongoose')
 const exphbs  = require('express-handlebars')
 const methodOverride = require('method-override') 
-const Todo = require('./models/todo')
+// 引用路由器
+const routes = require('./routes')
 
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
@@ -28,62 +29,7 @@ app.set('view engine', 'hbs')
 // 使用 body-parser 的 URL 編碼解析功能
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
-
-app.get('/',(req,res)=>{
-    Todo.find() // 取出 Todo model 裡的所有資料
-        .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
-        .sort({ _id: 'asc' }) // 根據 _id 升冪排序
-        .then(todos => res.render('index',{todos}))
-        .catch(error => console.error(error))
-})
-
-app.get('/todos/new', (req, res) => {
-    return res.render('new')
-  })
-
-app.post('/todos', (req, res) => {
-    const name = req.body.name.trim()       // 從 req.body 拿出表單裡的 name 資料
-    return Todo.create({ name })     // 存入資料庫
-      .then(() => res.redirect('/')) // 新增完成後導回首頁
-      .catch(error => console.log(error))
-  })
-
-  app.get('/todos/:id', (req, res) => {
-    const id = req.params.id
-    return Todo.findById(id)
-      .lean()
-      .then((todo) => res.render('detail', { todo }))
-      .catch(error => console.log(error))
-  })
-
-  app.get('/todos/:id/edit', (req, res) => {
-    const id = req.params.id
-    return Todo.findById(id)
-      .lean()
-      .then((todo) => res.render('edit', { todo }))
-      .catch(error => console.log(error))
-  })
-
-  app.put('/todos/:id', (req, res) => {
-    const id = req.params.id
-    const { name, isDone } = req.body
-    return Todo.findById(id)
-      .then(todo => {
-        todo.name = name
-        todo.isDone = isDone === 'on'
-        return todo.save()
-      })
-      .then(()=> res.redirect(`/todos/${id}`))
-      .catch(error => console.log(error))
-  })
-
-  app.delete('/todos/:id', (req, res) => {
-    const id = req.params.id
-    return Todo.findById(id)
-      .then(todo => todo.deleteOne())
-      .then(() => res.redirect('/'))
-      .catch(error => console.log(error))
-  })
+app.use(routes)
 
 app.listen(3000,()=>{
     console.log('App is running on http://localhost:3000.')
