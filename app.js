5e399d29115fc97728fc5670ec0a7390995e004c
tschiang23 <tien23@gmail.com>
2024-09-25 12:28:31 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,28 +1,11 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const exphbs  = require('express-handlebars')
 const methodOverride = require('method-override') 
 // 引用路由器
 const routes = require('./routes')
-
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
-  }
+require('./config/mongoose')
 
 const app = express()
-
-mongoose.connect(process.env.MONGODB_URI)
-
-  // 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 app.engine('hbs', exphbs.engine({defaultLayout: 'main',extname:'.hbs'}));
 app.set('view engine', 'hbs')
