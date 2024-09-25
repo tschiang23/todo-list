628759bb263e95b1b6d0b2a61db81c03ce9e06dc
tschiang23 <tien23@gmail.com>
2024-09-25 10:40:06 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/',(req,res)=>{
     Todo.find() // 取出 Todo model 裡的所有資料
         .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+        .sort({ _id: 'asc' }) // 根據 _id 升冪排序
         .then(todos => res.render('index',{todos}))
         .catch(error => console.error(error))
 })
