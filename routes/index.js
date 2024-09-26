657cbd078379de90db7454e485b712abf2c6ec2b
tschiang23 <tien23@gmail.com>
2024-09-26 14:30:31 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,14 @@ const router = express.Router()
 const home = require('./modules/home')
 // 引入 todos 模組程式碼
 const todos = require('./modules/todos')
+const users = require('./modules/users')
 
 
 // 將網址結構符合 / 字串的 request 導向 home 模組 
 router.use('/', home)
 // 將網址結構符合 /todos 字串開頭的 request 導向 todos 模組 
 router.use('/todos', todos)
+router.use('/users', users)
+
 // 匯出路由器
 module.exports = router
