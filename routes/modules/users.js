bb5005aed1453527d824990e86bb08aeb41eebb6
tschiang23 <tien23@gmail.com>
2024-09-30 15:24:41 +0800

    feat: add bcrypt.js

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const User = require('../../models/user')
 
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
 
 router.get('/login', (req, res) => {
   res.render('login')
@@ -63,18 +64,19 @@ router.post('/register', (req, res) => {
         password,
         confirmPassword
       })
-    } else {
-      // 如果還沒註冊：寫入資料庫
-      return User.create({
+    }
+    // 註冊使用者
+    return bcrypt
+      .genSalt(10) // 產生「鹽」，並設定複雜度係數為 10
+      .then(salt => bcrypt.hash(password, salt)) // 為使用者密碼「加鹽」，產生雜湊值
+      .then(hash => User.create({
         name,
         email,
-        password
+        password: hash // 用雜湊值取代原本的使用者密碼
       })
         .then(() => res.redirect('/'))
-        .catch(err => console.log(err))
-    }
+        .catch(err => console.log(err)))
   })
-    .catch(err => console.log(err))
 })
 
 module.exports = router
