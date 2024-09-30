2ba1f8f79ea1e01db18231469e508ad4f1d7c468
tschiang23 <tien23@gmail.com>
2024-09-30 09:52:53 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
-const exphbs  = require('express-handlebars')
-const methodOverride = require('method-override') 
+const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
+const session = require('express-session')
 // 引用路由器
 const routes = require('./routes')
 require('./config/mongoose')
@@ -9,13 +10,20 @@ const app = express()
 
 const port = process.env.PORT || 3000
 
-app.engine('hbs', exphbs.engine({defaultLayout: 'main',extname:'.hbs'}));
+app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', 'hbs')
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    // cookie: { secure: true }
+}))
 // 使用 body-parser 的 URL 編碼解析功能
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routes)
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`App is running on port:${port}.`)
 })
